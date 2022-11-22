7653a0cda46b768d0227af6e01274335f638738b
Raphael Batagini <raphabatagini@gmail.com>
2022-11-22 18:54:41 -0300

    chore: initialize repository

---
+++ README.md
@@ -0,0 +1,16 @@
+# Awesome Users Plugin
+
+## Plugin usage
+
+## Technologies
+
+## Interacting with plugin code
+
+1. Checking code standards:
+```sh
+$ vendor/bin/phpcs --standard="Inpsyde" <path>
+```
+Where <path> is at least one file or directory to check, e.g.:
+```sh
+$ vendor/bin/phpcs --standard="Inpsyde" ./src/ ./my-plugin.php
+```
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Awesome Users Plugin
+ * Plugin URI: https://github.com/RaphaelBatagini/awesome-users-plugin
+ * Description: Users listing plugin.
+ * Author: Raphael Batagini
+ * Author URI: https://github.com/RaphaelBatagini
+ * Version: 1.0.0
+ * License: GPLv2 or later
+ * Text Domain: awesome-users
+ * Domain Path: /languages/
+ */
