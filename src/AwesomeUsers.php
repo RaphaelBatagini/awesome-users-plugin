337eaaee8a00ea85c8e972ee12216d9778df4395
Raphael Batagini <raphabatagini@gmail.com>
2022-11-29 11:41:45 -0300

    refactor: improve class to generate virtual pages

--- src/AwesomeUsers.php
+++ src/AwesomeUsers.php
@@ -8,9 +8,13 @@ namespace RaphaelBatagini\AwesomeUsersPlugin;
 
 use RaphaelBatagini\AwesomeUsersPlugin\APIs\Users;
 use RaphaelBatagini\AwesomeUsersPlugin\Services\JsonPlaceholderUsers;
+use RaphaelBatagini\AwesomeUsersPlugin\Services\VirtualPage;
 
 final class AwesomeUsers
 {
+    private const AWESOME_USERS_PAGE_SLUG = 'my-awesome-users';
+    private const AWESOME_USERS_PAGE_TITLE = 'Awesome Users';
+
     private function __construct()
     {
     }
@@ -27,8 +31,23 @@ final class AwesomeUsers
 
     public function init()
     {
+        $this->initUsersApi();
+        $this->createUsersPage();
+    }
+
+    private function initUsersApi(): void
+    {
         $usersService = new JsonPlaceholderUsers();
         $usersApi = new Users($usersService);
         $usersApi->registerEndpoints();
+    }
+
+    private function createUsersPage(): void
+    {
+        new VirtualPage(
+            self::AWESOME_USERS_PAGE_SLUG,
+            self::AWESOME_USERS_PAGE_TITLE,
+            __DIR__ . '/../templates/users-list.php',
+        );
     }
 }
---
+++ src/Services/VirtualPage.php
@@ -0,0 +1,160 @@
+<?php
+
+// -*- coding: utf-8 -*-
+
+declare(strict_types=1);
+
+namespace RaphaelBatagini\AwesomeUsersPlugin\Services;
+
+use stdClass;
+
+class VirtualPage
+{
+    private string $pageSlug;
+    private string $pageTitle;
+    private string $pageTemplate;
+
+    public function __construct(string $slug, string $title, string $templatePath)
+    {
+        $this->pageSlug = $slug;
+        $this->pageTitle = $title;
+        $this->pageTemplate = $templatePath;
+
+        add_action('init', [$this, 'init']);
+    }
+
+    /**
+     * Hook to add the virtual page
+     */
+    public function init(): void
+    {
+        if ($this->getCurrentPageSlug() === $this->pageSlug) {
+            add_filter('the_posts', [$this, 'createPage'], 10, 2);
+        }
+    }
+
+    private function getCurrentPageSlug(): string
+    {
+        $requestUri = $this->getCurrentRequestUri();
+
+        if (get_option('permalink_structure')) {
+            return trim(parse_url($requestUri, PHP_URL_PATH), '/');
+        }
+
+        parse_str(parse_url($requestUri, PHP_URL_QUERY), $params);
+        return isset($params['page_id']) ? $params['page_id'] : false;
+    }
+
+    private function getCurrentRequestUri(): string
+    {
+        if (empty($_SERVER['REQUEST_URI'])) {
+            return '';
+        }
+
+        return esc_url_raw(wp_unslash($_SERVER['REQUEST_URI']));
+    }
+
+    /**
+     * @param $posts
+     * @param $query
+     * The virtual page filter
+     *
+     * @return stdClass[]
+     */
+    public function createPage(array $posts, \WP_Query $wpQuery): array
+    {
+        if (!empty($wpQuery->query['post_type'])) {
+            return $posts;
+        }
+
+        $post = $this->createPostObject();
+
+        // set filter results
+        $wpPost = new \WP_Post($post);
+        $posts = [$wpPost];
+        wp_cache_add($wpPost->ID, $wpPost, 'posts');
+
+        // reset wpQuery properties to simulate a found page
+        $wpQuery->post = $wpPost;
+        $wpQuery->posts = $posts;
+        $wpQuery->queried_object = $wpPost;
+        $wpQuery->queried_object_id = $wpPost->ID;
+        $wpQuery->found_posts = 1;
+        $wpQuery->post_count = 1;
+        $wpQuery->max_num_pages = 1;
+        $wpQuery->is_page = true;
+        $wpQuery->is_singular = true;
+        $wpQuery->is_single = false;
+        $wpQuery->is_attachment = false;
+        $wpQuery->is_archive = false;
+        $wpQuery->is_category = false;
+        $wpQuery->is_tag = false;
+        $wpQuery->is_tax = false;
+        $wpQuery->is_author = false;
+        $wpQuery->is_date = false;
+        $wpQuery->is_year = false;
+        $wpQuery->is_month = false;
+        $wpQuery->is_day = false;
+        $wpQuery->is_time = false;
+        $wpQuery->is_search = false;
+        $wpQuery->is_feed = false;
+        $wpQuery->is_comment_feed = false;
+        $wpQuery->is_trackback = false;
+        $wpQuery->is_home = false;
+        $wpQuery->is_embed = false;
+        $wpQuery->is_404 = false;
+        $wpQuery->is_paged = false;
+        $wpQuery->is_admin = false;
+        $wpQuery->is_preview = false;
+        $wpQuery->is_robots = false;
+        $wpQuery->is_posts_page = false;
+        $wpQuery->is_post_type_archive = false;
+
+        return $posts;
+    }
+
+    /**
+     * Generate the post object dynamically
+     *
+     * @return stdClass
+     */
+    private function createPostObject(): stdClass
+    {
+        $post = new \stdClass();
+        $post->ID = 0;
+        $post->post_author = 1;
+        $post->post_date = current_time('mysql');
+        $post->post_date_gmt = current_time('mysql', 1);
+        $post->post_content = $this->templateToString();
+        $post->post_title = $this->pageTitle;
+        $post->post_excerpt = '';
+        $post->post_status = 'publish';
+        $post->comment_status = 'closed';
+        $post->ping_status = 'closed';
+        $post->post_password = '';
+        $post->post_name = $this->pageSlug;
+        $post->to_ping = '';
+        $post->pinged = '';
+        $post->modified = $post->post_date;
+        $post->modified_gmt = $post->post_date_gmt;
+        $post->post_content_filtered = '';
+        $post->post_parent = 0;
+        $post->guid = get_home_url(1, '/' . $this->pageSlug);
+        $post->menu_order = 0;
+        $post->post_type = 'page';
+        $post->post_mime_type = '';
+        $post->comment_count = 0;
+        $post->filter = 'raw';
+
+        return $post;
+    }
+
+    private function templateToString(): string
+    {
+        ob_start();
+        include($this->pageTemplate);
+        $templateRendered = ob_get_contents();
+        ob_end_clean();
+        return $templateRendered;
+    }
+}
