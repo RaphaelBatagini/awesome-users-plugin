c1fdd44f802d95346137691b2152394416458f73
Raphael Batagini <raphabatagini@gmail.com>
2022-12-02 18:29:45 -0300

    fix: check if plugin root element exists before react render

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -3,8 +3,14 @@ import { render, createElement } from '@wordpress/element';
 import UserList from './components/user-list';
 
 domReady(function () {
+  const root = document.getElementById('awesome-users-plugin-app');
+
+  if (!root) {
+    return;
+  }
+
   render (
     createElement(UserList),
-    document.getElementById('awesome-users-plugin-app')
+    root
   );
 });
